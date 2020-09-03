0ffe62d8bf3166881f295ac69bcc417456748f79
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-09-03 18:50:11 -0300

    sistema enviando dados através do POST e adicionando no data.json já formatado

--- teachers.js
+++ teachers.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
+const data = require('./data.json')
 
 // create
 exports.post = function(req, res) {
     const keys = Object.keys(req.body)
 
-    fs.writeFile("data.json", JSON.stringify(data), function(err) {
+    for(key of keys) {
+        if (req.body[key] == "") {
+            return res.send('Preencha todos os campos!')
+        }
+    }
+
+    data.teachers.push(req.body);
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
         if (err) return res.send("write file error!")
         
         return res.redirect("/teachers")
